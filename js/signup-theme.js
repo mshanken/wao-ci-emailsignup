295d9e5348752ccc5714d239925da00be0aeea17
Edison Leon <eleon@mshanken.com>
2018-06-13 15:38:54 -0400

    checkbox update

--- js/signup-theme.js
+++ js/signup-theme.js
@@ -7,6 +7,11 @@ $(function () {
                 required: function(element) {
                     return $("#othersel").is(":selected");
                 }
+            },
+            CI_proactive_agree_to_receive_emails: {
+            	required: function(element) {
+            		return $("#CI_custom25").is(":checked");
+			    }
             }
 		},
 		validClass: "success",
@@ -15,7 +20,7 @@ $(function () {
 		submitHandler: function(form) { form.submit(); },
 		success: function(label, element){
 			// console.log( "test2: ", label, element );
-			var _this = $(element).parent();
+			var _this = ($(element).hasClass('form-check-input'))?$(element).parent().parent():$(element).parent();
 			if( $(_this).hasClass("has-error") ){
 				$(_this).removeClass("has-error").addClass("has-success");
 				$(_this).find("span.glyphicon").removeClass("glyphicon-remove").addClass("glyphicon-ok");
@@ -26,7 +31,7 @@ $(function () {
 			// console.log("test:", validator, event, validator.invalid);
 			$.each( validator.invalid, function( key, value ) {
 				// console.log( key + ": " + value );
-				var _this = $("#"+key).parent();
+				var _this = ($("#"+key).hasClass('form-check-input'))?$("#"+key).parent().parent():$("#"+key).parent();
 				$(_this).addClass("has-success");
 				$(_this).addClass("has-error has-feedback");
 				$(_this).find(".form-control-feedback").css("display","block");
@@ -69,12 +74,12 @@ $(function () {
         if( $(this).prop('checked') ){
         	$('#CI_subscribeForm').attr('action','//newsletters.mshanken.com/wc/wc7_verify.cfm');
         	$(this).val('Y');
-        	$('#CI_pproactive_agree_to_receive_emails').attr('required','required');
+        	// $('#CI_pproactive_agree_to_receive_emails').attr('required','required');
             $("#GDPRContent").removeClass('hidden');
         } else {
         	$('#CI_subscribeForm').attr('action','//newsletters.mshanken.com/wc/wc_verify.cfm');
         	$(this).val('N');
-        	$('#CI_pproactive_agree_to_receive_emails').removeAttr('required');
+        	// $('#CI_pproactive_agree_to_receive_emails').prop('required',false);
             $('#GDPRContent').addClass('hidden');
         }
     });
