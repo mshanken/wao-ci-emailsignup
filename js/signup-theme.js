7f6dfed3ff91554e7f51a3e739551d6b5f867346
Edison Leon <eleon@mshanken.com>
2019-07-25 15:54:26 -0400

    updating and validating new fields

--- js/signup-theme.js
+++ js/signup-theme.js
@@ -1,3 +1,23 @@
+'use strict';
+
+function populate() {
+    var checkboxes = document.getElementsByName('CI_custom29');
+    var ip1 = document.getElementById('CI_custom29');
+
+    ip1.value = '';
+
+    for (var i = 0, iLen = checkboxes.length; i < iLen; i++) {
+
+        if (checkboxes[i].checked) {
+            if (ip1.value.length === 0) {
+                ip1.value = checkboxes[i].value;
+            } else {
+                ip1.value = ip1.value + ',' + checkboxes[i].value;
+            }
+        }
+    }
+}
+
 $(function () {
 	$("#CI_subscribeForm").validate({
 		ignore: '.ignore',
@@ -6,6 +26,11 @@ $(function () {
             CI_custom7: {
                 required: function(element) {
                     return $("#othersel").is(":selected");
+                }
+            },
+            CI_custom30: {
+                required: function(element) {
+                    return $("#Other_for").is(":checked");
                 }
             },
             CI_proactive_agree_to_receive_emails: {
@@ -83,5 +108,33 @@ $(function () {
             $('#GDPRContent').addClass('hidden');
         }
     });
+
+    // check for Whisky interesr -- can not select more than 5 types
+	/*
+	 * For checkboxes instead, if use anyway
+	 */
+  	$('.row-two input').on('change', function(e) {
+		var counter = 0;
+  		$(".row-two input").each(function(index){
+	  		if($(this).prop('checked')){
+				counter++;
+			}
+		});
+		if(counter > 5){
+			$(this).attr('checked',false);
+			$(this).prev().removeClass('glyphicon-check').addClass('glyphicon-unchecked');
+			// $(this).prop('checked',false);
+			$("#myModal .modal-body").html("<p>You can not select more than 5 types</p>");
+			$("#myModal .modal-footer .btn").text("close");
+			$("#myModal").modal("show");
+		}
+		if( $(this).prop('checked') && $(this).val() === "Other_for" && counter <= 5 ){
+			$(".another-hidden-field").removeClass('hide');
+		} else {
+			$(".another-hidden-field").addClass('hide');
+		}
+	});
+
+
 
 });
